dc0bb9fcffef5fb2d49dfb192ed7a121bc30a30a
Yun Jae Shin <joeyshin@sas.upenn.edu>
2023-02-09 17:19:42 -0500

    Initial Project Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './components/Landing.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Create a header for the project and call it "Projether" */}
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Row, Col, Button } from "react-bootstrap";
+import "./Landing.css";
+
+
+const Landing = () => {
+  return (
+    <Container className="landing-page">
+      <Row className="justify-content-center">
+        <Col xs={12} sm={10} md={8} lg={6} xl={4}>
+          <h1 className="text-center one-liner">
+            A Place to Find and Fund Projects
+          </h1>
+          <Button variant="primary" size="lg" block>
+            Let's Begin
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Landing
